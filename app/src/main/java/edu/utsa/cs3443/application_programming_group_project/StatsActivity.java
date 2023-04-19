779b88c03d15c3fbd9d7fff71ed8f70a3572afb9
mthorn824 <91444261+mthorn824@users.noreply.github.com>
2023-04-19 15:02:35 -0500

    views
    Added the different views

---
+++ app/src/main/java/edu/utsa/cs3443/application_programming_group_project/StatsActivity.java
@@ -0,0 +1,14 @@
+package edu.utsa.cs3443.application_programming_group_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StatsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stats);
+    }
+}
