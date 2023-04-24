9b0606d3292f838481f887441d99164715c21cbf
L Grissom <126531867+laurgris@users.noreply.github.com>
2023-04-24 15:41:48 -0500

    laurens more awesome commit

---
+++ app/src/main/java/edu/utsa/cs3443/application_programming_group_project/StatsController.java
@@ -0,0 +1,24 @@
+package edu.utsa.cs3443.application_programming_group_project;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.view.View;
+
+public class StatsController implements View.OnClickListener {
+    MainActivity mainActivity;
+    private AssetManager am;
+    private Context c;
+
+    public StatsController(Context context, MainActivity mainActivity){
+        c = context;
+        this.mainActivity = mainActivity;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(mainActivity, StatsActivity.class);
+
+        mainActivity.startActivity(intent);
+    }
+}
